1f385177f4b7116399a8558956ee4a90e0fdb40c
Joao Pedro Martins <joaop.congiomartins@gmail.com>
2019-03-06 04:22:48 -0300

    add blogs url, blogs_home with db fetch

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from . import models
+
+def blogs_home(request):
+    blogs = models.Blog.objects
+    return render(request, 'blog/blogs_home.html', {'blogs': blogs, 'teste': [1,2,3] })
